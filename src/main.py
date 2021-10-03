8f8f0f14230bdd7323463d6bb9dd295e160982f9
Keiz <vanookay@mail.ru>
2021-10-03 14:24:25 +0700

    Added logging to file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import logging
 from typing import Any
 
 from fastapi import FastAPI
 
 from src.api.routers import api_router
 from src.db.database import Base, engine
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(filename='info.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.WARNING)
 
 # Создание таблиц в БД
 Base.metadata.create_all(bind=engine)
